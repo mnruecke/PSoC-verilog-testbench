cd530f53ba990b19c01cb42f8b3003df5f26d4fc
Martin A. Rueckert <martin.rueckert@physik.uni-wuerzburg.de>
2023-02-06 22:14:03 +0100

    add uart rx example in verilog (testbench code is incomplete)

--- basic_testbench.cydsn/main.c
+++ basic_testbench.cydsn/main.c
@@ -20,6 +20,9 @@
 // Declare byte array in flash memory:
 static const char CYCODE flash_userdata[230000]={0};
 
+void connect_to_example_1(void);
+void connect_to_example_2(void);
+
 CY_ISR_PROTO( isr_rx_handle );
 
 int main(void)
@@ -45,6 +48,12 @@ int main(void)
 
 CY_ISR( isr_rx_handle )
 { 
+    connect_to_example_2();
+    
+    LED_Write(1); CyDelay(50); LED_Write(0); CyDelay(50);
+}//END CY_ISR( isr_rx_handle )
+
+void connect_to_example_1(void){
     char putty = UART_1_GetChar();
     if( putty == 'x' ) CySoftwareReset();
     
@@ -53,10 +62,42 @@ CY_ISR( isr_rx_handle )
     
     // tb data out
     UART_1_PutChar( o_Testbench8_MSB_Read() );
-    UART_1_PutChar( o_Testbench8_LSB_Read() );
+    UART_1_PutChar( o_Testbench8_LSB_Read() );    
+}//END connect_to_example_1()
+
+
+int char_in_count=0;   
+char i_clock=0;
+char i_rx=0;
+void connect_to_example_2(void){
     
-    LED_Write(1); CyDelay(50); LED_Write(0); CyDelay(50);
-}//END CY_ISR( isr_rx_handle )
+    char byte_in = UART_1_GetChar();
+    
+    // Reset condition
+    if( byte_in == 'x' && (i_clock == 'x' || i_rx == 'x')){
+        char_in_count   = 0;
+        i_clock         = 0;
+        i_rx            = 0;        
+    }else if( char_in_count%2 == 0){
+        i_clock = byte_in;
+        ++char_in_count;
+    }else{
+        i_rx = UART_1_GetChar();
+        ++char_in_count;
+    }
+    
+    i_Uart_TB_clock_Write( i_clock );
+    i_Uart_TB_rx_Write(    i_rx    );
+
+    UART_1_PutChar( char_in_count );
+    UART_1_PutChar( i_clock );
+    UART_1_PutChar( i_rx );
+    
+    UART_1_PutChar( o_Uart_TB_RX_DV_Read() );
+    UART_1_PutChar( o_Uart_TB_RX_Byte_Read() );
+    
+    
+}//END connect_to_example_1()
 
 
 /* [] END OF FILE */
--- run_hardware_test_bench_via_uart.py
+++ run_hardware_test_bench_via_uart.py
@@ -15,27 +15,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-""" main settings """
+""" main settings and definitions """
 
 # Serial port:
 serialPort = '\\\\.\\COM10'
 baudrate = 115200
 time_out = 1
 
-
-# Testbench input data
-i_tb_input_lsb  = 0b_1010_1111
-
-
-""" END - main settings """
-
-
-""" interact with testbench on PSoC """
-try: # open and interact with serial port 
-
-    ser = serial.Serial( serialPort, baudrate, timeout=time_out )
-    ser.flushInput()
+# Test routines
+def run_example_1():
+    # Testbench input data, basic example
     
+    i_tb_input_lsb  = 0b_1010_1111
     
     for repetition_i in range( 1 ):
     
@@ -51,8 +42,60 @@ try: # open and interact with serial port
         o_tb_output_lsb = int.from_bytes( o_tb_output_lsb, 'big' )
         print( "tb output port lsb:\t\t{}".format( bin(o_tb_output_lsb)))
         
-        #print( "expected output: \t\t{}".format( bin( i_tb_input_lsb**2 )))
-                
+        #print( "expected output: \t\t{}".format( bin( i_tb_input_lsb**2 )))    
+
+def run_example_2():
+    # Testbench input data, uart rx example
+    
+    # 1) Reset psoc routine
+    def reset_test_routine():
+        ser.write( b'x' )   
+        o_byte = ser.read(5)
+        return o_byte
+    
+    o_byte = reset_test_routine()
+    if o_byte[0] != 0:
+        o_byte = reset_test_routine()
+
+    print( o_byte[0],
+           o_byte[1],
+           o_byte[2],
+           o_byte[3],
+           o_byte[4],
+    )
+    
+    # 2) Generate test pattern for uart rx
+    clock = 0
+    for half_clk in range(5):
+        
+        
+        if o_byte[0] % 2 == 0:
+            ser.write( bytes([clock]))
+            clock   = not clock
+            o_byte_ = ser.read(5) 
+        else:
+            ser.write( b'\x01' )   
+            o_byte = ser.read(5)     
+            
+        print( o_byte[0],
+               o_byte[1],
+               o_byte[2],
+               o_byte[3],
+               o_byte[4],
+        )
+
+
+""" END - main settings and definitions """
+
+
+""" interact with testbench on PSoC """
+try: # open and interact with serial port 
+
+    ser = serial.Serial( serialPort, baudrate, timeout=time_out )
+    ser.flushInput()
+    time.sleep( 0.05 )
+    
+    run_example_2()            
 
 finally: # close serial port
     ser.close()
